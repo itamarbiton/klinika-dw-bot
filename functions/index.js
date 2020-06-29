164ba178feef9f3a0b6617da09331365dd6ecc6a
Itamar Biton <itamar.biton@climacell.co>
2020-06-29 11:09:36 +0300

    Fixed some bugs, first production version

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,106 @@ if (Object.keys(functions.config()).length) config = functions.config();
 
 const start_message = `ברוך הבא עבד צעיר!\nהגעת לבוט התורנויות של הקליניקה, הלא היא דירה 2, בן יהודה 48 א׳, תל אביב-יפו`;
 
+async function findTask(taskId) {
+  let taskQueryRef = db.collection('mesimot').where('id', '==', taskId);
+  let taskQuerySnapshot;
+  try {
+    taskQuerySnapshot = await taskQueryRef.get();
+  } catch (err) {
+    throw err;
+  }
+
+  if (taskQuerySnapshot.docs.length === 0) {
+    throw new Error(`could not find task with identifier ${taskId}`)
+  }
+
+  let taskDocSnapshot = taskQuerySnapshot.docs[0]
+  return { snapshpt: taskDocSnapshot, data: taskDocSnapshot.data() };
+}
+
+async function findUser(userId) {
+  let userRef = db.collection('users').doc(userId);
+
+  let userSnapshot;
+  try {
+    userSnapshot = await userRef.get();
+  } catch (err) {
+    throw err;
+  }
+
+  if (!userSnapshot.exists) throw new Error('failed to find a user with the supplied identifier');
+  return { snapshot: userSnapshot, data: userSnapshot.data() };
+}
+
+async function rotateTask(task, taskId) {
+  if (task.rotation === undefined) {
+    return console.log(`task ${task.name} was not initialized yet, did not rotate`);
+  }
+
+  if (task.currentIndex === task.rotation.length - 1) {
+    task.currentIndex = 0
+  } else {
+    task.currentIndex += 1;
+  }
+  task.rotateLastUpdate = new Date();
+
+  try {
+    await db.collection('mesimot').doc(taskId).set(task);
+  } catch (err) {
+    return console.log(`failed to update task with identifier ${taskId}`);
+  }
+
+  return undefined;
+}
+
+async function handleRotation(ctx = undefined) {
+  let tasksQuerySnapshot;
+  try {
+    tasksQuerySnapshot = await db.collection('mesimot').get();
+  } catch (err) {
+    return console.log(`failed to get tasks from database ${err}`);
+  }
+
+  let rotatePromises = tasksQuerySnapshot.docs.map(docSnapshot => rotateTask(docSnapshot.data(), docSnapshot.id));
+
+  try {
+    let results = await Promise.all(rotatePromises);
+    console.log(results);
+  } catch (err) {
+    return console.log(`failed to rotate tasks, ${err}`);
+  }
+
+  if (ctx !== undefined) {
+    ctx.reply(`סיבוב תורנויות בוצע בהצלחה!`);
+  }
+
+  return undefined;
+}
+
+async function handleInform() {
+  let tasksQuerySnapshot;
+  try {
+    tasksQuerySnapshot = await db.collection('mesimot').get();
+  } catch (err) {
+    return console.log(`failed to get tasks from database, ${err}`);
+  }
+
+  for (const taskDocSnapshot of tasksQuerySnapshot.docs) {
+    let task = taskDocSnapshot.data();
+
+    if (task.rotation === undefined) {
+      console.log(`the task ${task.name} was not initialized yet, did not inform`);
+      continue;
+    }
+
+    let assigneeId = task.rotation[task.currentIndex];
+    let informMsgStr = `בוקר טוב, היום תורך במשימה *״${task.name}״*, דיר בלאק...`;
+    bot.telegram.sendMessage(assigneeId, informMsgStr, Extra.markup().markdown())
+  }
+
+  return undefined;
+}
+
 async function handleStart(ctx) {
   let senderId = ctx.from.id.toString();
   let userDocRef = db.collection('users').doc(senderId);
@@ -23,8 +123,7 @@ async function handleStart(ctx) {
   try {
     await userDocRef.set({
       chatId: ctx.chat.id,
-      firstName: ctx.from.first_name,
-      lastName: ctx.from.last_name,
+      firstName: ctx.from.first_name
     });
   } catch (err) {
     return console.log('failed to register user in the database, ' + err);
@@ -55,17 +154,15 @@ async function handleMesimot(ctx) {
 
 async function handleAni(ctx) {
   let senderId = ctx.from.id.toString();
-  let userRef = db.collection('users').doc(senderId);
 
-  let userSnapshot;
+  let user;
   try {
-    userSnapshot = await userRef.get();
+    const result = await findUser(senderId);
+    user = result.data;
   } catch (err) {
-    return console.log(`failed to get user with identifier ${ctx.from.id} from database`);
+    return console.log('failed to find a user with the supplied identifier');
   }
 
-  if (!userSnapshot.exists) return console.log('failed to find a user with the supplied identifier');
-  let user = userSnapshot.data();
   let userName = user.displayName || user.firstName;
   let replyMsgStr = `שם התצוגה שלך הוא *${userName}*`;
 
@@ -84,21 +181,21 @@ async function handleShem(ctx) {
 
   let newName = nameParts.join(' ');
   let senderId = ctx.from.id.toString();
-  let userRef = db.collection('users').doc(senderId);
 
-  let userSnapshot;
+  let snapshot;
+  let user;
   try {
-    userSnapshot = await userRef.get();
+    const result = await findUser(senderId);
+    snapshot = result.snapshot;
+    user = result.data;
   } catch (err) {
-    return console.log('failed to update the user\'s name, ' + err);
+    return console.log(`failed to find user with identifier ${senderId}, ${err}`);
   }
 
-  if (!userSnapshot.exists) return console.log(`failed to find user with identifier ${senderId}`);
-  let user = userSnapshot.data();
   user.displayName = newName;
 
   try {
-    await userRef.set(user);
+    await db.collection('users').doc(senderId).set(user);
   } catch (err) {
     return console.log(`failed to update the user's name, ${err}`);
   }
@@ -117,22 +214,16 @@ async function handleAvadim(ctx) {
     return console.log('failed to get the tasks identifier');
   }
 
-  let taskQuery = db.collection('mesimot').where('id', '==', taskId);
-
-  let taskQuerySnapshot;
+  let snapshot;
+  let task;
   try {
-    taskQuerySnapshot = await taskQuery.get();
+    const result = await findTask(taskId);
+    snapshot = result.snapshpt;
+    task = result.data;
   } catch (err) {
-    return console.log(`failed to get task with identifier ${taskId} from database ${err}`);
+    ctx.reply(`לא נמצאה משימה עם המספר המבוקש, לצפייה ברשימת המשימות שלח /mesimot`);
+    return console.log(`failed to find task with identifier ${taskId}`);
   }
-
-  if (taskQuerySnapshot.docs.length === 0) {
-    ctx.reply('לא נמצאה משימה עם המספר שנשלח, כדי לראות את המשימות הזמינות הקלד /mesimot');
-    return console.log(`failed to find a task with identifier ${taskId}`);
-  }
-
-  let taskDocSnapshot = taskQuerySnapshot.docs[0];
-  let task = taskDocSnapshot.data();
 
   let assigneeIds = Object.keys(task.assignees);
   let assigneeRefs = assigneeIds.map(assigneeId => db.collection('users').doc(assigneeId));
@@ -147,9 +238,130 @@ async function handleAvadim(ctx) {
   assigneeDocSnapshots = assigneeDocSnapshots.filter(docSnapshot => docSnapshot.exists);
   let assignees = assigneeDocSnapshots.map(docSnapshot => docSnapshot.data());
   let assigneeDescArr = assignees.map(assignee => `👷🏻‍♂️ ${assignee.displayName || assignee.firstName}`);
-  let replyMsgStr = assigneeDescArr.join('\n');
+
+  let msgTitleStr = `להלן רשימת העבדים עבור המשימה ״${task.name}״ -\n`;
+  let replyMsgStr = msgTitleStr + assigneeDescArr.join('\n');
 
   ctx.reply(replyMsgStr);
+  return undefined;
+}
+
+async function handleAniEved(ctx) {
+  let msgParts = ctx.message.text.split(' ');
+  let taskId = parseInt(msgParts[1]);
+  let senderId = ctx.from.id.toString();
+
+  if (isNaN(taskId)) {
+    ctx.reply(`יש להכניס מספר משימה, לקבלת המשימות הזמינות שלח /mesimot`);
+    return console.log(`did not receive task identifier`);
+  }
+
+  let snapshot
+  let task;
+  try {
+    const result = await findTask(taskId);
+    snapshot = result.snapshpt;
+    task = result.data;
+  } catch (err) {
+    ctx.reply(`לא נמצאה משימה עם המספר המבוקש, לצפייה ברשימת המשימות שלח /mesimot`);
+    return console.log(`failed to find task with identifier ${taskId}`);
+  }
+
+  task.assignees[senderId] = true
+
+  try {
+    await db.collection('mesimot').doc(snapshot.id).set(task);
+  } catch (err) {
+    return console.log(`failed to update task with identifier ${taskId}, ${err}`);
+  }
+
+  ctx.reply(`נרשמת בהצלחה למשימה "${task.name}"`);
+  return undefined;
+}
+
+async function createTaskSikumString(task) {
+  if (task.rotation === undefined) {
+    return `🔴 המשימה ״${task.name}״ *טרם אותחלה*`;
+  }
+
+  let assigneeId = task.rotation[task.currentIndex];
+
+  let snapshot;
+  let assignee;
+  try {
+    const result = await findUser(assigneeId);
+    snapshot = result.snapshot;
+    assignee = result.data;
+  } catch (err) {
+    throw new Error(`failed to find user with identifier ${assigneeId}, ${err}`);
+  }
+
+  return `⚪️ התורן היומי למשימה ״${task.name}״ הוא *${assignee.displayName || assignee.firstName}*`;
+}
+
+async function handleSikum(ctx) {
+  let tasksCollectionRef = db.collection('mesimot');
+
+  let tasksQuerySnapshot;
+  try {
+    tasksQuerySnapshot = await tasksCollectionRef.get();
+  } catch (err) {
+    return console.log(`failed to get the list of tasks from the databse, ${err}`);
+  }
+
+  let tasks = tasksQuerySnapshot.docs
+    .filter(snapshot => snapshot.exists)
+    .map(snapshot => snapshot.data());
+
+  let tasksDescArr;
+  try {
+    tasksDescArr = await Promise.all(tasks.map(task => createTaskSikumString(task)));
+  } catch (err) {
+    return console.log(`failed to create task descriptions, ${err}`);
+  }
+
+  let msgTitleStr = `להלן סיכום המשימות היומי -\n`;
+  let replyMsgStr = msgTitleStr + tasksDescArr.join(`\n`);
+  ctx.reply(replyMsgStr, Extra.markup().markdown());
+  return undefined;
+}
+
+async function handleYalla(ctx) {
+  let messageParts = ctx.message.text.split(' ');
+  let taskId = parseInt(messageParts[1]);
+
+  if (isNaN(taskId)) {
+    ctx.reply('יש להעביר את מספר המשימה, למשימות הזמינות שלח /mesimot');
+    return console.log('no task identifier was received')
+  }
+
+  let snapshot;
+  let task;
+  try {
+    const result = await findTask(taskId);
+    snapshot = result.snapshpt;
+    task = result.data;
+  } catch (err) {
+    ctx.reply(`לא נמצאה משימה עם המספר המבוקש, לרשימת המשימות שלח /mesimot`);
+    return console.log(`failed to find task with identifier ${taskId}, ${err}`);
+  }
+
+  if (Object.keys(task.assignees).length === 0) {
+    ctx.reply('לא ניתן להתחיל משימה שלא נרשמה אליה עבדים!');
+    return conslog.log('can\'t start a task without assignees');
+  }
+
+  task.rotation = Object.keys(task.assignees);
+  task.currentIndex = 0
+  task.rotateLastUpdate = new Date();
+
+  try {
+    await db.collection('mesimot').doc(snapshot.id).set(task);
+  } catch (err) {
+    return console.log(`failed to initialize task with identifier ${taskId}`);
+  }
+
+  ctx.reply('המשימה אותחלה בהצלחה!');
   return undefined;
 }
 
@@ -160,6 +372,13 @@ bot.command('mesimot', async (ctx) => await handleMesimot(ctx));
 bot.command('ani', async (ctx) => await handleAni(ctx));
 bot.command('shem', async (ctx) => await handleShem(ctx));
 bot.command('avadim', async (ctx) => await handleAvadim(ctx));
+bot.command('anieved', async (ctx) => await handleAniEved(ctx));
+bot.command('sikum', async (ctx) => await handleSikum(ctx));
+bot.command('dorotate', async (ctx) => handleRotation(ctx));
+bot.command('doinform', async (ctx) => handleInform());
+bot.command('yalla', async (ctx) => handleYalla(ctx));
+
+// bot.launch();
 
 exports.botHandler = functions.https.onRequest(async (req, res) => {
   try {
